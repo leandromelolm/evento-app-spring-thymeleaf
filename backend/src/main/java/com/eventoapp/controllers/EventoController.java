cbc5d2fa1fc2a296b52630dd45ea5da65cda8dbb
Leandro Melo <leandrosiqueira.pe@gmail.com>
2022-07-21 16:11:19 -0300

    Criado requisicao especificas com a anotação @PathVariable. Add Pagina detalhesEvento

--- backend/src/main/java/com/eventoapp/controllers/EventoController.java
+++ backend/src/main/java/com/eventoapp/controllers/EventoController.java
@@ -2,6 +2,7 @@ package com.eventoapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -33,4 +34,13 @@ public class EventoController {
 		mv.addObject("eventos", eventos);
 		return mv;
 	}
+	
+	@RequestMapping("/{codigo}")
+	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) {
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		Evento evento = er.findByCodigo(codigo);
+		mv.addObject("evento", evento);
+		return mv;
+	}
+	
 }
