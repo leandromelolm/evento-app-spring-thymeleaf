c658c1c03209642f46ee1f8a08d2c9eb5971b5de
Leandro <leandrosiqueira.pe@gmail.com>
2022-07-28 08:14:34 -0300

    Test codigo javacript em documento externo

---
+++ backend/src/main/resources/static/js/test.js
@@ -0,0 +1,20 @@
+var currYear = (new Date()).getFullYear();
+
+$(document).ready(function() {
+	$(".datepicker").datepicker({
+		defaultDate: new Date(currYear - 5, 1, 31),
+		// setDefaultDate: new Date(2000,01,31),
+		maxDate: new Date(currYear - 5, 12, 31),
+		format: "dd/mm/yyyy",
+		autoClose: true,
+		showDaysInNextAndPreviousMonths: true
+	});
+});
+
+function alertaSair() {	
+	alert("Deslogado");
+}
+
+function clickEvent() {	
+	alert("Test");	
+}  
