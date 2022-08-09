c1fe47af26c650eaeb8d386e9ec1fabdcc40a2ff
Leandro Melo <leandrosiqueira.pe@gmail.com>
2022-08-09 15:59:07 -0300

    Alteracao detalhesEvento input cpf

--- backend/src/main/resources/static/js/init.js
+++ backend/src/main/resources/static/js/init.js
@@ -6,7 +6,7 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-function mascara() {
-	$('#telefoneCelular').mask('(00) 0 0000-0000');
-	$('#cpf').mask('000.000.000-00'); // Mascara funciona na versão mobile
-};
+//function mascara() {
+//	$('#telefoneCelular').mask('(00) 0 0000-0000');
+//	$('#cpf').mask('000.000.000-00'); // Mascara funciona na versão mobile
+//};
